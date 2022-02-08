3dd09c1f6d22b8f8d24b3a98854376e162a0d96a
alex9127git <mail.alex9127@yandex.ru>
2022-02-08 16:15:00 +0300

    Added Moving and Deleting modes, you can delete and move already built buildings now

--- Assets/Scripts/ModeSwitcherScript.cs
+++ Assets/Scripts/ModeSwitcherScript.cs
@@ -3,14 +3,14 @@ using UnityEngine.UI;
 
 public class ModeSwitcherScript : MonoBehaviour
 {
-    string[] modes = {"Spying Mode"};
-    public int modeType = 0;
+    string[] modes = {"Spying Mode", "Moving Mode", "Deleting Mode"};
+    public static int modeType = 0;
     public Text textField;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Tab))
+        if (Input.GetKeyDown(KeyCode.Tab) && !MouseFollowScript.busy)
         {
             modeType += 1;
             modeType %= modes.Length;
