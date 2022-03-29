d7f38798cdba5018dc52c139b4be301a0858b2f1
alex9127git <mail.alex9127@yandex.ru>
2022-03-29 15:57:54 +0300

    I made the .bat file to commit my changes more interactive
    with waiting for user and interactive output

--- Assets/commit_changes.bat
+++ Assets/commit_changes.bat
@@ -1,4 +1,9 @@
+@echo off
 git status
+echo These are the files going to be commited. Press any key to commit the files
+pause
 git add .
 git commit
 git push
+echo Commit done. Press any key to close the window
+pause
